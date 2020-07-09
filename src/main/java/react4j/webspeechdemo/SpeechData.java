57f021b29db483e65e5718e61df5def7eac639ec
Peter Donald <peter@realityforge.org>
2020-07-09 19:47:12 +1000

    Expose list of voices

--- src/main/java/react4j/webspeechdemo/SpeechData.java
+++ src/main/java/react4j/webspeechdemo/SpeechData.java
@@ -1,13 +1,30 @@
 package react4j.webspeechdemo;
 
+import arez.ComputableValue;
+import arez.annotations.Action;
 import arez.annotations.ArezComponent;
+import arez.annotations.ComputableValueRef;
+import arez.annotations.DepType;
 import arez.annotations.Feature;
+import arez.annotations.Memoize;
 import arez.annotations.Observable;
+import arez.annotations.OnActivate;
+import arez.annotations.OnDeactivate;
+import elemental2.core.JsArray;
+import elemental2.dom.DomGlobal;
+import elemental3.EventListener;
+import elemental3.SpeechSynthesis;
+import elemental3.SpeechSynthesisVoice;
+import java.util.Arrays;
+import java.util.List;
 import javax.annotation.Nonnull;
 
 @ArezComponent
 abstract class SpeechData
 {
+  @Nonnull
+  private final EventListener _eventListener = e -> triggerRecalculation( getVoicesComputableValue() );
+
   @SuppressWarnings( "SameParameterValue" )
   @Nonnull
   static SpeechData create( final double pitch, final double rate, final double volume )
@@ -29,4 +46,39 @@ abstract class SpeechData
   abstract double getVolume();
 
   abstract void setVolume( double volume );
+
+  @Memoize( depType = DepType.AREZ_OR_EXTERNAL )
+  List<SpeechSynthesisVoice> getVoices()
+  {
+    final JsArray<SpeechSynthesisVoice> voices = getSpeechSynthesis().getVoices();
+    return Arrays.asList( voices.asArray( new SpeechSynthesisVoice[ voices.length ] ) );
+  }
+
+  //TODO: This should not be present because we pass it in via onVoicesActivate
+  @ComputableValueRef
+  abstract ComputableValue<?> getVoicesComputableValue();
+
+  @OnActivate
+  void onVoicesActivate()
+  {
+    getSpeechSynthesis().addEventListener( "voiceschanged", _eventListener );
+  }
+
+  @OnDeactivate
+  void onVoicesDeactivate()
+  {
+    getSpeechSynthesis().removeEventListener( "voiceschanged", _eventListener );
+  }
+
+  @Action
+  void triggerRecalculation( @Nonnull final ComputableValue<?> computableValue )
+  {
+    computableValue.reportPossiblyChanged();
+  }
+
+  @Nonnull
+  private SpeechSynthesis getSpeechSynthesis()
+  {
+    return WebSpeechWindow.of( DomGlobal.window ).speechSynthesis();
+  }
 }
