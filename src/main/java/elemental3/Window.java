ba32655ec93b0bd6e704528e2d79645cbdb4e5b2
Peter Donald <peter@realityforge.org>
2020-07-14 22:32:33 +1000

    Import generated variant

--- src/main/java/elemental3/Window.java
+++ src/main/java/elemental3/Window.java
@@ -1,5 +1,6 @@
 package elemental3;
 
+import javax.annotation.Generated;
 import javax.annotation.Nonnull;
 import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
@@ -7,25 +8,29 @@ import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 import jsinterop.base.Js;
 
-// TODO: This should be generated. It is also unclear why webtack did not generate an error
-//  when it found un processed partial
-@JsType( isNative = true, name = "Window", namespace = JsPackage.GLOBAL )
-public final class Window
-{
+@Generated("org.realityforge.webtack")
+@JsType(
+  isNative = true,
+  namespace = JsPackage.GLOBAL,
+  name = "Window"
+)
+public final class Window {
+  /**
+   * Use the static of method to cast an existing object to this type.
+   */
+  @Deprecated
+  private Window() {
+  }
+
   @JsOverlay
   @Nonnull
-  public static Window of( @Nonnull final Object o )
-  {
-    return Js.cast( o );
+  public static Window of(@Nonnull final Object object) {
+    return Js.cast( object );
   }
 
-  // Constructor is only present to support the java compiler
-  @Deprecated
-  private Window()
-  {
-  }
-
-  @JsProperty( name = "speechSynthesis" )
+  @JsProperty(
+    name = "speechSynthesis"
+  )
   @Nonnull
   public native SpeechSynthesis speechSynthesis();
 }
