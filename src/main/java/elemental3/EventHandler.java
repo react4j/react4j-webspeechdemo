07be75f280b23e640f5b0e58bdf60a45483804ae
Peter Donald <peter@realityforge.org>
2020-08-06 06:45:25 +1000

    Regenerate from webtack

--- src/main/java/elemental3/EventHandler.java
+++ src/main/java/elemental3/EventHandler.java
@@ -2,14 +2,11 @@ package elemental3;
 
 import javax.annotation.Generated;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import jsinterop.annotations.JsFunction;
-import jsinterop.base.Any;
 
 @Generated("org.realityforge.webtack")
 @JsFunction
 @FunctionalInterface
 public interface EventHandler {
-  @Nullable
-  Any onInvoke(@Nonnull Event event);
+  void onInvoke(@Nonnull Event event);
 }
