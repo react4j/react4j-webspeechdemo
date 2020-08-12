e3589bd48b71c486e982f715783f071327e8e002
Peter Donald <peter@realityforge.org>
2020-08-12 22:58:37 +1000

    Regenerate from webtack

--- src/main/java/elemental3/EventListener.java
+++ src/main/java/elemental3/EventListener.java
@@ -5,6 +5,11 @@ import javax.annotation.Nonnull;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
+/**
+ * The EventListener interface represents an object that can handle an event dispatched by an EventTarget object.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventListener">EventListener - MDN</a>
+ */
 @Generated("org.realityforge.webtack")
 @JsType(
     isNative = true,
@@ -13,5 +18,10 @@ import jsinterop.annotations.JsType;
 )
 @FunctionalInterface
 public interface EventListener {
+  /**
+   * The EventListener method handleEvent() method is called by the user agent when an event is sent to the EventListener, in order to handle events that occur on an observed EventTarget.
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventListener/handleEvent">EventListener.handleEvent - MDN</a>
+   */
   void handleEvent(@Nonnull Event event);
 }
--- src/main/java/elemental3/EventTarget.java
+++ src/main/java/elemental3/EventTarget.java
@@ -6,6 +6,11 @@ import javax.annotation.Nullable;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
+/**
+ * EventTarget is a DOM interface implemented by objects that can receive events and may have listeners for them.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget">EventTarget - MDN</a>
+ */
 @Generated("org.realityforge.webtack")
 @JsType(
     isNative = true,
@@ -16,21 +21,56 @@ public class EventTarget {
   public EventTarget() {
   }
 
+  /**
+   * The EventTarget method addEventListener() sets up a function that will be called whenever the specified event is delivered to the target.
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener">EventTarget.addEventListener - MDN</a>
+   */
   public native void addEventListener(@Nonnull String type, @Nullable EventListener callback,
       @Nonnull AddEventListenerOptions options);
 
+  /**
+   * The EventTarget method addEventListener() sets up a function that will be called whenever the specified event is delivered to the target.
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener">EventTarget.addEventListener - MDN</a>
+   */
   public native void addEventListener(@Nonnull String type, @Nullable EventListener callback,
       boolean options);
 
+  /**
+   * The EventTarget method addEventListener() sets up a function that will be called whenever the specified event is delivered to the target.
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener">EventTarget.addEventListener - MDN</a>
+   */
   public native void addEventListener(@Nonnull String type, @Nullable EventListener callback);
 
+  /**
+   * Dispatches an Event at the specified EventTarget, (synchronously) invoking the affected EventListeners in the appropriate order. The normal event processing rules (including the capturing and optional bubbling phase) also apply to events dispatched manually with dispatchEvent().
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/dispatchEvent">EventTarget.dispatchEvent - MDN</a>
+   */
   public native boolean dispatchEvent(@Nonnull Event event);
 
+  /**
+   * The EventTarget.removeEventListener() method removes from the EventTarget an event listener previously registered with EventTarget.addEventListener(). The event listener to be removed is identified using a combination of the event type, the event listener function itself, and various optional options that may affect the matching process; see Matching event listeners for removal
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener">EventTarget.removeEventListener - MDN</a>
+   */
   public native void removeEventListener(@Nonnull String type, @Nullable EventListener callback,
       @Nonnull EventListenerOptions options);
 
+  /**
+   * The EventTarget.removeEventListener() method removes from the EventTarget an event listener previously registered with EventTarget.addEventListener(). The event listener to be removed is identified using a combination of the event type, the event listener function itself, and various optional options that may affect the matching process; see Matching event listeners for removal
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener">EventTarget.removeEventListener - MDN</a>
+   */
   public native void removeEventListener(@Nonnull String type, @Nullable EventListener callback,
       boolean options);
 
+  /**
+   * The EventTarget.removeEventListener() method removes from the EventTarget an event listener previously registered with EventTarget.addEventListener(). The event listener to be removed is identified using a combination of the event type, the event listener function itself, and various optional options that may affect the matching process; see Matching event listeners for removal
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener">EventTarget.removeEventListener - MDN</a>
+   */
   public native void removeEventListener(@Nonnull String type, @Nullable EventListener callback);
 }
