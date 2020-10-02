40f93097bd71a9f5e610ddb2f188f4c46a1ef424
Peter Donald <peter@realityforge.org>
2020-10-02 20:32:56 +1000

    Regenerate

--- src/main/java/elemental3/speech/SpeechSynthesisErrorCode.java
+++ src/main/java/elemental3/speech/SpeechSynthesisErrorCode.java
@@ -1,10 +1,12 @@
 package elemental3.speech;
 
+import java.lang.annotation.Documented;
 import javax.annotation.Generated;
 import javax.annotation.Nonnull;
 import org.intellij.lang.annotations.MagicConstant;
 
 @Generated("org.realityforge.webtack")
+@Documented
 @MagicConstant(
     valuesFromClass = SpeechSynthesisErrorCode.class
 )
